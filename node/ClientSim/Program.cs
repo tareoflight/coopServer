742efc98c7e44a00111363b0016e02e71ce8b78e
dosfx <andrew.adbrown@gmail.com>
2025-03-19 19:25:56 -0400

    basic socket back and forth

---
+++ node/ClientSim/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Starting node client");
+
+IPAddress addr = IPAddress.Parse("127.0.0.1");
+int port = 25569;
+IPEndPoint endpoint = new(addr, port);
+
+using Socket socket = new(addr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+await socket.ConnectAsync(endpoint);
+while (true)
+{
+    string message = "Hi friends 👋!<|EOM|>";
+    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+    await socket.SendAsync(messageBytes, SocketFlags.None);
+    Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+    byte[] buffer = new byte[1_024];
+    int received = await socket.ReceiveAsync(buffer, SocketFlags.None);
+    string response = Encoding.UTF8.GetString(buffer, 0, received);
+    if (response == "<|ACK|>") {
+        Console.WriteLine($"Socket Client received acknowledgement: \"{response}\"");
+        break;
+    }
+}
+
+socket.Shutdown(SocketShutdown.Both);
---
+++ node/NodeServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Starting node server");
+
+IPAddress addr = IPAddress.Parse("127.0.0.1");
+int port = 25569;
+IPEndPoint endpoint = new(addr, port);
+
+using Socket socket = new(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+Console.WriteLine("binding...");
+socket.Bind(endpoint);
+socket.Listen(100);
+
+Socket handler = await socket.AcceptAsync();
+const string eom = "<|EOM|>";
+const string ack = "<|ACK|>";
+
+while (true)
+{
+    byte[] buffer = new byte[1_024];
+    int recieved = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    string response = Encoding.UTF8.GetString(buffer, 0, recieved);
+
+    if (response.Contains(eom))
+    {
+        Console.WriteLine($"Socket server received message: \"{response.Replace(eom, "")}\"");
+        byte[] ackBytes = Encoding.UTF8.GetBytes(ack);
+        await handler.SendAsync(ackBytes, 0);
+
+        Console.WriteLine($"Socket server sent acknowledgment: \"{ack}\"");
+    }
+}
