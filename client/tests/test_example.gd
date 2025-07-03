753aa0fe6d0f565fa6ec76b080514b3d55a9816f
Nathan <nathan.rochon@gmail.com>
2025-07-02 20:56:47 -0400

    adding a simple connect

--- client/tests/test_example.gd
+++ client/tests/test_example.gd
@@ -1,6 +1,15 @@
 extends Node
 
+var client := StreamPeerTCP.new()
+var reponce := preload("res://Scripts/grpc/response.gd")
+
 func _ready():
+	var err = client.connect_to_host("127.0.0.1", 25569)
+	if err == OK:
+		print("Connecting to server...")
+	else:
+		print("Connection failed: ", err)
+		
 	print("-----------------UNIT TESTS-------------")
 
 	var tests = {
@@ -13,9 +22,23 @@ func _ready():
 		tests[name].call()
 	
 
+func connect_on(port : int = 25569):
+	
+	pass
+
+
 func send_request():
-	# TODO: Implement mock test
-	print("send_request test skipped.")
+	if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+		# Example: Send a message
+		var message = reponce.new()
+		
+		var data = message.send_request()
+		client.put_data(data)
+
+		# Try to read response (non-blocking)
+		if client.get_available_bytes() > 0:
+			var received = client.get_utf8_string(client.get_available_bytes())
+			print("Received from server: ", received)
 
 func get_response():
 	# TODO: Implement mock test
