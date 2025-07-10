1e7b6717ba542cd7fb5246eb9e42ca05de0b96c0
Nathan <nathan.rochon@gmail.com>
2025-07-09 22:01:19 -0400

    IT LIVESS

--- client/tests/test_example.gd
+++ client/tests/test_example.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var client := StreamPeerTCP.new()
-var reponce := preload("res://Scripts/grpc/response.gd")
+var reponce := preload("res://Scripts/grpc/comms.gd")
 
 func _ready():
 	var err = client.connect_to_host("127.0.0.1", 25569)
@@ -28,18 +28,34 @@ func connect_on(port : int = 25569):
 
 
 func send_request():
+	while client.get_status() == StreamPeerTCP.STATUS_CONNECTING:
+		client.poll()
+		print("connecting")
+		await get_tree().create_timer(1).timeout
+		
+		
 	if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
 		# Example: Send a message
-		var message = reponce.new()
+		var message = reponce.Request.new()
+		message.set_request_id(1)
+		message.new_control_request()
+		message.get_control_request().new_shutdown()
+		message.get_control_request().get_shutdown().set_delay(1)
+		var data = message.to_bytes()
 		
-		var data = message.send_request()
+		print("--------------------")
+		print(data)
+		print(message)
+		print("+++++++++++++++++++")
+		client.put_32(len(data))
 		client.put_data(data)
-
 		# Try to read response (non-blocking)
 		if client.get_available_bytes() > 0:
 			var received = client.get_utf8_string(client.get_available_bytes())
 			print("Received from server: ", received)
-
+	else:
+		print("not connected",client.get_status())
+		
 func get_response():
 	# TODO: Implement mock test
 	print("get_response test skipped.")
